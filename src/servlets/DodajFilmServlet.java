8ffd63386182d20decc9d3e75a1a28aec3064999
JoHnY <janstezalski@gmail.com>
2017-12-14 23:46:29 +0100

    Dodałem możlowość dodawania filmów przez pracownika

--- src/servlets/DodajFilmServlet.java
+++ src/servlets/DodajFilmServlet.java
@@ -15,51 +15,72 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 @WebServlet("/dodajFilm")
 public class DodajFilmServlet extends HttpServlet {
 
+    /**
+     * Metoda umożliwia dodanie filmu do bazy. W razie potrzeby tworzy również nowe gatunki filmu.
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Pobieranie danych z formularza na temat filmu
         String tytul = request.getParameter("tytul");
         String rokProdukcji = request.getParameter("rokProdukcji");
         String opis = request.getParameter("opis");
-        String[] newGatunek = request.getParameterValues("gatunek");
+        String[] gat = request.getParameterValues("gatunek");
 
+        // Tworzenie Regex-ów
         String patternPusty = ".+";
         String patternOpis = ".{1,255}";
         String patternRokProdukcji = "19[0-9]{2}|20[0-9]{2}";
 
+        // Pominięcie dodawania jeśli wchodzimy w dodawanie pierwszy raz
         if (tytul != null) {
+
+            // Sprawdzenie czy dane zostały poprawnie wstawione
             if (Pattern.matches(patternPusty, tytul) && Pattern.matches(patternPusty, rokProdukcji)
                     && Pattern.matches(patternRokProdukcji, rokProdukcji) && Pattern.matches(patternOpis, opis)) {
+
+                // Dodanie danych o filmie
                 Film film = new Film();
                 film.setTytul(tytul);
                 film.setRokProdukcji(Integer.parseInt(rokProdukcji));
                 film.setOpis(opis);
                 film.setDataDodania(new Timestamp(new Date().getTime()));
                 FilmDAO filmDAO = new FilmDAO();
+
+                // Dodanie nowego filmu
                 filmDAO.addFilm(film);
 
+                GatunekFilmDAO gatunekFilmDAO = new GatunekFilmDAO();
                 GatunekDAO gatunekDAO = new GatunekDAO();
                 Gatunek gatunek;
-                GatunekFilm newGatunekFilm = new GatunekFilm();
-                GatunekFilmDAO gatunekFilmDAO = new GatunekFilmDAO();
 
-                for (int i = 0; i < newGatunek.length; i++) {
-                    if (Pattern.matches(patternPusty, newGatunek[i])) {
+                // Pozbycie się powtórzeń w gatunkach za pomocą zbioru
+                Set<String> newGatunek = new HashSet<>(Arrays.asList(gat));
+
+                // Iteracja po podanych gatunkach
+                for (String g: newGatunek) {
+                    g = g.substring(0, 1).toUpperCase() + g.substring(1);
+                    // Sprawdzenie czy gatunek nie jest pusty
+                    if (Pattern.matches(patternPusty, g)) {
                         try {
                             // Szukanie gatunku w bazie
-                            gatunek = gatunekDAO.getWybranyGatunekPoNazwie(newGatunek[i]);
+                            gatunek = gatunekDAO.getWybranyGatunekPoNazwie(g);
                         } catch (NoResultException e) {
                             // Tworzenie nowego gatunku
                             gatunek = new Gatunek();
-                            gatunek.setNazwa(newGatunek[i]);
+                            gatunek.setNazwa(g);
                             gatunekDAO.addGatunek(gatunek);
                         }
                         // Dodanie nowego połączenia gatunekFilm
-                        newGatunekFilm.setIdGatunku(gatunekDAO.getId());
+                        GatunekFilm newGatunekFilm = new GatunekFilm();
+                        newGatunekFilm.setIdGatunku(gatunekDAO.getWybranyGatunekPoNazwie(g).getIdGatunku());
                         newGatunekFilm.setIdFilmu(filmDAO.getId());
                         gatunekFilmDAO.addGatunekFilm(newGatunekFilm);
                     }
