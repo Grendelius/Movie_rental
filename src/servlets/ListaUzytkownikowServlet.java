bfc77f41dc0779cf64c8f7eae8fd4c94923e4a04
JoHnY <janstezalski@gmail.com>
2018-01-14 22:32:23 +0100

    Naprawiłem usuwanie użytkowników

---
+++ src/servlets/ListaUzytkownikowServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import dao.UzytkownicyDAO;
+import models.Uzytkownik;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/listaUzytkownikow")
+public class ListaUzytkownikowServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String akcja = request.getParameter("akcja");
+
+        if (akcja != null) {
+            String idUzytkownika = request.getParameter("idUzytkownika");
+            UzytkownicyDAO uzytkownicyDAO = new UzytkownicyDAO();
+            Uzytkownik uzytkownik = uzytkownicyDAO.getUzytkownikPoId(Integer.parseInt(idUzytkownika));
+            if (akcja.equals("Zablokuj konto")) {
+                uzytkownik.setZablokowany(true);
+                uzytkownicyDAO.updateUzytkownika(uzytkownik);
+            } else if (akcja.equals("Odblokuj konto")) {
+                uzytkownik.setZablokowany(false);
+                uzytkownicyDAO.updateUzytkownika(uzytkownik);
+            } else if (akcja.equals("Usuń konto")) {
+                uzytkownicyDAO.deleteUzytkownika(uzytkownik);
+            } else {
+                // Zmiana roli
+                uzytkownik.setRola(akcja);
+                uzytkownicyDAO.updateUzytkownika(uzytkownik);
+            }
+        }
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getAttribute("czyListaUzytkownikow") == null) {
+            request.setAttribute("czyListaUzytkownikow", "true");
+        }
+        request.getRequestDispatcher("/WEB-INF/views/panelUzytkownika.jsp").forward(request, response);
+    }
+
+
+}
--- src/servlets/PanelUzytkownikaServlet.java
+++ src/servlets/PanelUzytkownikaServlet.java
@@ -1,6 +1,5 @@
 package servlets;
 
-import dao.UzytkownicyDAO;
 import models.Uzytkownik;
 
 import javax.servlet.ServletException;
@@ -20,29 +19,9 @@ public class PanelUzytkownikaServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         Uzytkownik zalogowany = (Uzytkownik) request.getSession().getAttribute("uzytkownik");
-        String akcja = request.getParameter("akcja");
 
         if (zalogowany != null)
-            if (akcja != null) {
-                String idUzytkownika = request.getParameter("idUzytkownika");
-                UzytkownicyDAO uzytkownicyDAO = new UzytkownicyDAO();
-                Uzytkownik uzytkownik = uzytkownicyDAO.getUzytkownikPoId(Integer.parseInt(idUzytkownika));
-                if (akcja.equals("Zablokuj konto")) {
-                    uzytkownik.setZablokowany(true);
-                    uzytkownicyDAO.updateUzytkownika(uzytkownik);
-                } else if (akcja.equals("Odblokuj konto")) {
-                    uzytkownik.setZablokowany(false);
-                    uzytkownicyDAO.updateUzytkownika(uzytkownik);
-                } else if (akcja.equals("Usuń konto")) {
-                    uzytkownicyDAO.deleteUzytkownika(uzytkownik);
-                } else {
-                    // Zmiana roli
-                    uzytkownik.setRola(akcja);
-                    uzytkownicyDAO.updateUzytkownika(uzytkownik);
-                }
-                doGet(request, response);
-            } else
-                doGet(request, response);
+            doGet(request, response);
         else {
             request.setAttribute("blad", "<br> Dostęp do Panelu użytkownika mają tylko zalogowane osoby!");
             request.getRequestDispatcher("/login").forward(request, response);
@@ -52,4 +31,6 @@ public class PanelUzytkownikaServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/panelUzytkownika.jsp").forward(request, response);
     }
+
+
 }
